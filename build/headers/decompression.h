c451efdfded149ec7422577c702ecd1e3bf2abab
flash-computer <zaiberinteractive@gmail.com>
2025-05-01 05:54:27 -0700

    My bad fams

--- build/headers/decompression.h
+++
@@ -1,7 +0,0 @@
-#include"swftag.h"
-#include"error.h"
-#include<stdio.h>
-
-err movie_uncomp(FILE *swf, pdata *state);
-err movie_deflate(FILE *swf, pdata *state);
-err movie_lzma(FILE *swf, pdata *state);
